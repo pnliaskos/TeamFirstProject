b8c98c9574996dbd1bb1a2a8884ae9981e9b87c7
Valentinos Geroutskis <valentinosgr@gmail.com>
2023-03-06 20:59:02 +0200

    Add getters and setters

--- src/main/java/gr/kariera/mindthecode/teamfirstproject/Order.java
+++ src/main/java/gr/kariera/mindthecode/teamfirstproject/Order.java
@@ -15,5 +15,28 @@ public class Order {
     private Float discount;
     @ManyToMany
     Set<Product> orderProducts;
-// getters setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Float getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Float discount) {
+        this.discount = discount;
+    }
+
+    public Set<Product> getOrderProducts() {
+        return orderProducts;
+    }
+
+    public void setOrderProducts(Set<Product> orderProducts) {
+        this.orderProducts = orderProducts;
+    }
 }
--- src/main/java/gr/kariera/mindthecode/teamfirstproject/Product.java
+++ src/main/java/gr/kariera/mindthecode/teamfirstproject/Product.java
@@ -17,5 +17,36 @@ public class Product {
 
     @ManyToMany
     Set<Order> productOrders;
-// getters and setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Order> getProductOrders() {
+        return productOrders;
+    }
+
+    public void setProductOrders(Set<Order> productOrders) {
+        this.productOrders = productOrders;
+    }
 }
