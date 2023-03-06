8c74008e7bc062e6a092e8c1510bb99b6b8b7d65
Panagiotis Liaskos <pnliaskos@gmail.com>
2023-03-06 20:33:27 +0200

    Initial commit

---
+++ src/main/java/gr/kariera/mindthecode/teamfirstproject/TeamFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package gr.kariera.mindthecode.teamfirstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamFirstProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamFirstProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gr/kariera/mindthecode/teamfirstproject/TeamFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.kariera.mindthecode.teamfirstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamFirstProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
